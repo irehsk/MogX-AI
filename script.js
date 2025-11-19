b11120365ef3ace349d2c1a368442a250b129491
irehsk <shauryatechno31@gmail.com>
2025-11-19 16:26:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-const chatScreen = document.getElementById("chat-screen");
-
-function openChat() {
-    chatScreen.style.display = "block";
-    setTimeout(() => {
-        chatScreen.style.opacity = "1";
-    }, 20);
-}
-
-function closeChat() {
-    chatScreen.style.opacity = "0";
-    setTimeout(() => {
-        chatScreen.style.display = "none";
-    }, 500);
+function scrollToChat() {
+    document.querySelector("#chat").scrollIntoView({
+        behavior: "smooth"
+    });
 }
